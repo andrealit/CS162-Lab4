ca27a8253558b629c792cd9d33e49cee5918776c
Andrea Tongsak <atongsak@gmail.com>
2020-07-07 13:30:23 -0700

    Initial Commit

---
+++ CS162-Lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS162-Lab4
+//
+//  Created by Andrea Tongsak on 7/7/20.
+//  Copyright © 2020 Andrea Tongsak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
